52375e06f18eb65781bfd93546cdd5ca2ab4ff57
utkarshpatil646 <112856877+utkarshpatil646@users.noreply.github.com>
2026-03-06 12:13:44 +0530

    Remove duplicate helm repo commands in steps.sh

--- jenkins/templates/steps.sh
+++ jenkins/templates/steps.sh
@@ -1,6 +1,7 @@
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+ssh node01 << EOF
 sudo mkdir -p /mnt/data/jenkins
 sudo chown -R 1000:1000 /mnt/data/jenkins
 sudo chmod -R 775 /mnt/data/jenkins
-
-helm repo add jenkins https://charts.jenkins.io
-helm repo update
+EOF
