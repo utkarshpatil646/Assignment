f5ccdd32c29c67fd5d7b76c5a9b223c6db535528
utkarshpatil646 <112856877+utkarshpatil646@users.noreply.github.com>
2026-03-06 12:17:20 +0530

    Create steps.sh

---
+++ steps.sh
@@ -0,0 +1,12 @@
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+ssh node01 << EOD
+sudo mkdir -p /mnt/data/jenkins
+sudo chown -R 1000:1000 /mnt/data/jenkins
+sudo chmod -R 775 /mnt/data/jenkins
+EOD
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install monitoring prometheus-community/kube-prometheus-stack
+kubectl create secret generic postgres-exporter-secret --from-literal=DATA_SOURCE_NAME="postgresql://appuser:apppassword@postgress-custom-postgres:5432/appdb?sslmode=disable"kubectl get secret monitoring-grafana -o jsonpath="{.data.admin-password}" | base64 --decode
