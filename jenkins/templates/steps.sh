c1c2279c6b7e30d2dffa70dbace92c7ae6bab639
utkarshpatil646 <112856877+utkarshpatil646@users.noreply.github.com>
2026-03-06 12:15:01 +0530

    Change EOF to EOD in steps.sh script

--- jenkins/templates/steps.sh
+++ jenkins/templates/steps.sh
@@ -1,7 +1,7 @@
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-ssh node01 << EOF
+ssh node01 << EOD
 sudo mkdir -p /mnt/data/jenkins
 sudo chown -R 1000:1000 /mnt/data/jenkins
 sudo chmod -R 775 /mnt/data/jenkins
-EOF
+EOD
